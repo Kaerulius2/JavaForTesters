1e6578d2b48035c1fb3d5c3eadeb6502305b959f
Alexander Golubkov <kaerulius2@yandex.ru>
2019-04-22 12:51:42 +0300

    Создан тест для создания контакта. Параметризован.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+    private final String firstname;
+    private final String midname;
+    private final String lastname;
+    private final String address;
+    private final String homephone;
+    private final String email;
+
+    public UserData(String firstname, String midname, String lastname, String address, String homephone, String email) {
+        this.firstname = firstname;
+        this.midname = midname;
+        this.lastname = lastname;
+        this.address = address;
+        this.homephone = homephone;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMidname() {
+        return midname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomephone() {
+        return homephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
