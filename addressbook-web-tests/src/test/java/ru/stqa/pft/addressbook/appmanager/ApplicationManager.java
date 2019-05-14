91b24a446e3ccfe12257d04d1b66129dd7299973
Alexander Golubkov <kaerulius2@yandex.ru>
2019-05-14 17:52:18 +0300

    Отработан запуск в разных браузерах.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,9 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,10 +14,25 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser=browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
-        wd = new ChromeDriver();
+
+
+        if(browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
+            wd = new ChromeDriver();
+        }else if(browser.equals(BrowserType.FIREFOX)){
+            System.setProperty("webdriver.gecko.driver", "C:\\Geckodriver\\geckodriver.exe");
+            wd = new FirefoxDriver();
+        }else if (browser.equals(BrowserType.EDGE)){
+            System.setProperty("webdriver.edge.driver", "C:\\Edgedriver\\MicrosoftWebDriver.exe"); //C:\Edgedriver
+            wd = new EdgeDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
