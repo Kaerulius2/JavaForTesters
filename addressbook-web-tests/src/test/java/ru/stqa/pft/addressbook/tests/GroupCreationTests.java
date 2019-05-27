306bf842c2528a707279ee2ec9d42c51fb657d17
Alexander Golubkov <kaerulius2@yandex.ru>
2019-05-27 19:45:36 +0300

    Создан провайдер тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,18 +4,31 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
 
-    @Test
-    public void testGroupCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("TestGroup1").withHeader("Header_1").withFooter("Footer_1")});
+        list.add(new Object[] {new GroupData().withName("TestGroup2").withHeader("Header_2").withFooter("Footer_2")});
+        list.add(new Object[] {new GroupData().withName("TestGroup3").withHeader("Header_3").withFooter("Footer_3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
 
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("TestGroup2");
+
         app.group().create(group);
 
         assertThat(app.group().count(),equalTo(before.size()+1));
@@ -23,7 +36,7 @@ public class GroupCreationTests extends TestBase {
         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
     }
 
-    @Test
+    @Test(enabled = false)
     public void testBadGroupCreation() throws Exception {
 
         app.goTo().groupPage();
