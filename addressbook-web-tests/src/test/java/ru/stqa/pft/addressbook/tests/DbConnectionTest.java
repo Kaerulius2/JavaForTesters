e89867a815b49f3fd12b467728537bae3ca6abb0
Kaerulius2 <kaerulius2@yandex.ru>
2019-05-28 22:28:16 +0300

    Добавлен коннектор БД

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class DbConnectionTest {
+
+    @Test
+    public void testDbConnection(){
+        
+
+    }
+}
