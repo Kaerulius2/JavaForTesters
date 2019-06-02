782a0b9e8ccc563cb5a557a2af137e1cf9408e72
Alexander Golubkov <kaerulius2@yandex.ru>
2019-06-02 16:00:34 +0300

    Получение почты на внутренний почтовый сервер.

--- mantis-tests/src/test/java/ru/stqa/ptf/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/ptf/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
     private String browser;
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
+    private MailHelper mailHelper;
 
 
     public ApplicationManager(String browser){
@@ -56,6 +57,13 @@ public class ApplicationManager {
             ftp = new FtpHelper(this);
         }
         return ftp;
+    }
+
+    public MailHelper mail(){
+        if(mailHelper == null){
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
     }
 
     public RegistrationHelper registration() {
