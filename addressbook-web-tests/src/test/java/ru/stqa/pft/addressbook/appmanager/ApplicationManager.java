a60c5baff9b4eaf3d563f4375108c2018010fa05
Alexander Golubkov <kaerulius2@yandex.ru>
2019-06-13 17:21:24 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,14 +1,18 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -34,15 +38,22 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
         DbHelper = new DbHelper();
 
-        if(browser.equals(BrowserType.CHROME)) {
-            System.setProperty("webdriver.chrome.driver", properties.getProperty("path.chromedriver"));
-            wd = new ChromeDriver();
-        }else if(browser.equals(BrowserType.FIREFOX)){
-            System.setProperty("webdriver.gecko.driver", properties.getProperty("path.firefoxdriver"));
-            wd = new FirefoxDriver();
-        }else if (browser.equals(BrowserType.EDGE)){
-            System.setProperty("webdriver.edge.driver", properties.getProperty("path.edgedriver")); //C:\Edgedriver
-            wd = new EdgeDriver();
+        if("".equals(properties.getProperty("selenium.server"))) {
+
+            if (browser.equals(BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("path.chromedriver"));
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.gecko.driver", properties.getProperty("path.firefoxdriver"));
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.EDGE)) {
+                System.setProperty("webdriver.edge.driver", properties.getProperty("path.edgedriver")); //C:\Edgedriver
+                wd = new EdgeDriver();
+            }
+        }else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
         }
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
